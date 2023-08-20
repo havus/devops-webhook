6a054101662a45b789e22ccce3d3c93e35f0da4f
Hafidz Mahrus <havus.it@gmail.com>
2023-08-20 20:51:58 +0800

    checkpoint

--- domain_checker.sh
+++ domain_checker.sh
@@ -4,6 +4,23 @@
 
 IFS=',' read -ra split_array <<< "$1"
 
+prev_val_ip=""
+all_same=true
+
 for domain in "${split_array[@]}"; do
-  echo $domain $(dig $domain A +short)
+  temp_val_ip=$(dig $domain A +short)
+
+  echo $domain $temp_val_ip
+
+  if [[ $all_same && $prev_val_ip != "" && "$prev_val_ip" != "$temp_val_ip" ]]; then
+    all_same=false
+  fi
+
+  prev_val_ip=$temp_val_ip
 done
+
+if $all_same; then
+  echo "Everything is okay"
+else
+  echo "WARNING: Please re-check all domain have same ip!"
+fi
